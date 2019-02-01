96326af3721321a0ae7c657978695c820207fa0a
RafaMateos <rafaelmateostorres@gmail.com>
2019-02-01 14:31:51 +0100

    Puta Mierda

--- hamburguer-menu-v2/src/app/components/orders/orders.component.ts
+++ hamburguer-menu-v2/src/app/components/orders/orders.component.ts
@@ -34,7 +34,6 @@ export class OrdersComponent implements OnInit {
   }
 
   Borrar(id){
-    
     this.miOrderService.deleteOrder(id).subscribe(result=>{
       console.log(result)
       this.Pedidos = this.miOrderService.getInfoPrduct();
